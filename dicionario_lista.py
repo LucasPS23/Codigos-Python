6d669fa07ce0f613dc22668e1d863e270a0e3ada
LucasPS23 <lucasan2011@gmail.com>
2025-03-31 16:24:09 -0300

    Add files via upload

---
+++ dicionario_lista.py
@@ -0,0 +1,26 @@
+clientes = list()
+pessoa = dict()
+soma = média = 0
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    clientes.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-='*30)
+print(f'Ao todo temos {len(clientes)} pessoas cadastradas.')
+média = soma/len(clientes)
+print(f'A média de idade é de {média:5.2f} anos')
